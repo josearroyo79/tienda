c8c4b25cb1b43ade67265ddb1a6d8ee86058c72d
Raul <raulbohonal06@gmail.com>
2020-01-15 09:08:30 +0100

    controlador

--- admin/usuarios/controladores/ControladorAcceso.php
+++ admin/usuarios/controladores/ControladorAcceso.php
@@ -1,26 +1,14 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 require_once CONTROLLER_PATH."ControladorBD.php";
 
 class ControladorAcceso {
-    // Variable instancia para Singleton
     static private $instancia = null;
-    
-    // constructor--> Private por el patrón Singleton
+
     private function __construct() {
         //echo "Conector creado";
     }
 
-    /**
-     * Patrón Singleton. Ontiene una instancia de controlador
-     * @return instancia del controlador
-     */
     public static function getControlador() {
         if (self::$instancia == null) {
             self::$instancia = new ControladorAcceso();
@@ -29,43 +17,34 @@ class ControladorAcceso {
     }
     
     public function salirSesion() {
-        // Recuperamos la información de la sesión
         session_start();
-        // Y la eliminamos las variables de la sesión y coockies
+
         unset($_SESSION['USUARIO']);
-        //unset($_COOKIE['CONTADOR']);
-        // ahora o las borramos todas o las destruimos, yo haré todo para que se vea
+
         session_unset();
         session_destroy();
     }
     
     
     public function procesarIdentificacion($email, $password){
-
-            //borro la sesion, por gusto, no se debería hacer pues nos cargamos todas
-           // $this->salierSesion();
-
-           //Aquí decimos que la contraseña del login va encriptada en sha256:
             $password = hash("sha256",$password);
 
-            // Conectamos a la base de datos
+            // Conexion BBDD
             $bd = ControladorBD::getControlador();
             $bd->abrirBD();
-            // creamos la consulta pero esta vez paremtrizada
+            
             $consulta = "SELECT * FROM usuarios WHERE email=:email and password=:password";
             $parametros = array(':email' => $email, ':password' => $password);
-            // Obtenemos las filas directamente como objetos con las columnas de la tabla
+            
             $res = $bd->consultarBD($consulta,$parametros);
             $filas=$res->fetchAll(PDO::FETCH_OBJ);
-            //var_dump($filas);
+            
             if (count($filas) > 0) {
-                 // abrimos las sesiones
-                 //session_start();
-                 // Almacenamos el usuario en la sesion.
-                 $_SESSION['USUARIO']['email']=$email;
-                 //echo $_SESSION['USUARIO']['email'];
-                 header("location: ../index.php"); 
-                 exit();              
+                 
+                $_SESSION['USUARIO']['email']=$email;
+                
+                header("location: ../index.php"); 
+                exit();              
             } else {
                 echo "<div class='wrapper'>";
                     echo "<div class='container-fluid'>";
@@ -81,12 +60,9 @@ class ControladorAcceso {
                         echo "</div>";
                     echo "</div>";
                 echo "</div>";
-                //<!-- Pie de la página web -->
+                // Pie web
                 require_once VIEW_PATH."pie.php";
                 exit();
             }
     }
-    
-    
-
 }
--- admin/usuarios/controladores/ControladorImagen.php
+++ admin/usuarios/controladores/ControladorImagen.php
@@ -1,27 +1,18 @@
 <?php
-    // Incluimos los ficheros que ncesitamos
+
     require_once $_SERVER['DOCUMENT_ROOT'] . "/tienda/admin/dirs.php";
     require_once CONTROLLER_PATH."ControladorUsuario.php";
     require_once UTILITY_PATH."funciones.php";
 
-
-    /**
-     * Controlador de descargas
-     */
+    
 class ControladorImagen {
     
-    // Variable instancia para Singleton
     static private $instancia = null;
-
-    // constructor--> Private por el patrón Singleton
+    
     private function __construct() {
         //echo "Conector creado";
     }
     
-    /**
-     * Patrón Singleton. Ontiene una instancia del Controlador de Descargas
-     * @return instancia de conexion
-     */
     public static function getControlador() {
         if (self::$instancia == null) {
             self::$instancia = new ControladorImagen();
@@ -39,7 +30,7 @@ class ControladorImagen {
     function eliminarImagen($imagen) {
         $fichero = IMAGE_PATH . $imagen;
         if (file_exists($fichero)) {
-            unlink($fichero); // Funcion para borrar desde el servidor
+            unlink($fichero);
             return true;
             //throw new Exception('No se puede borrar el fichero ' . $fichero . ' Por favor cierre otras aplicaciones que lo pueden estar usando.');
         }
@@ -48,7 +39,6 @@ class ControladorImagen {
     
     function actualizarImagen(){
         $imagenAnterior = trim($_POST["imagenAnterior"]);
-        // Procesamos la imagen
         $extension = explode("/", $_FILES['imagen']['type']);
         $nombreImagen = md5($_FILES['imagen']['tmp_name'] . $_FILES['imagen']['name']) . "." . $extension[1];
         if (!move_uploaded_file($_FILES['imagen']['tmp_name'], ROOT_PATH . "imagenes/$nombreImagen")) {
