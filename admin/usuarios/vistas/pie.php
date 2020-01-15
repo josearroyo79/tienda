b71fd0c131e93132bbc1f4bd6b7d6beb729bfa6b
Raul <raulbohonal06@gmail.com>
2020-01-15 10:08:05 +0100

    catalogo+login+pie

--- admin/usuarios/vistas/pie.php
+++ admin/usuarios/vistas/pie.php
@@ -1,6 +1,5 @@
 </body>
 <footer class="page-footer font-small blue">
-
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3">Realizado por:
         <a href="#"> Raúl Muñoz Fernández - 2º ASIR</a>
